7cdd6c744bac27f532224cc406f6ac5fca8dd8c8
Sergey Kopysov <syrkop@gmail.com>
2019-09-14 12:14:46 +0400

    Create DooBee.java

---
+++ DooBee.java
@@ -0,0 +1,17 @@
+$ java BeeDoo
+DooBeeDooBeeDo
+
+# Впиши недостающий код
+public class DooBee{
+  public static void main(String[] args){
+    int x = 1;
+    while (x < ____ ){
+      System.out.________ ("Doo");
+      System.out.________ ("Bee");
+      x = x + 1;
+    }
+    if (x == ____){
+      System.out.________ ("Do");
+    }
+  }
+}
