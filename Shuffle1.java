b150b9bc8ceacad3b250e8fbab7ec72991a89c21
Sergey Kopysov <syrkop@gmail.com>
2019-09-14 12:32:17 +0400

    Create Shuffle1.java

---
+++ Shuffle1.java
@@ -0,0 +1,34 @@
+$ java Shuffle1
+a-b c-d
+
+
+//кусочек 1
+if (x == 1) {
+  System.out.print("d");
+  x = x - 1;
+}
+
+//кусочек 2
+if (x == 2) {
+  System.out.print("b c");
+}
+
+//кусочек 3
+class Shuffle1 {
+  public static void main(String [] args){
+  
+  
+//кусочек 4
+if (x > 2) {
+  System.out.print("a");
+}
+
+//кусочек 5
+int x = 3;
+
+//кусочек 6
+x = x -1;
+System.out.print("-");
+
+//кусочек 7
+while (x > 0){
